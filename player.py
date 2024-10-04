4a8705c9354ae90356c93330aa2058cf748c2f54
CodeWithFranco <120446758+CodeWithFranco@users.noreply.github.com>
2024-10-03 20:13:27 -0700

    Initial release

--- player.py
+++ player.py
@@ -4,4 +4,15 @@ FINISH_LINE_Y = 280
 
 
 class Player:
-    pass
+    def __init__(self):
+        super().__init__()
+        self.speed(1)
+        self.color("white")
+        self.shape("turtle") 
+        self.penup()
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        go_up = self.ycor() + MOVE_DISTANCE
+        self.goto(0, go_up)
+    
